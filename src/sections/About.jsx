6c3e94938744fee074a2bcb6264727e87c5f42bf
abhijitc18 <chouguleabhijit18@gmail.com>
2025-11-05 18:23:41 +0530

    ui: modified components for better design consistency

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,7 @@
 // src/sections/About.jsx
 import React from "react";
 import "../styles/About.css";
-import aboutImg from "../assets/vishal_yadav.jpeg";
+import aboutImg from "../assets/about-us.jpg";
 import { FaLinkedin, FaInstagram, FaFacebook } from "react-icons/fa";
 
 const About = () => (
@@ -15,18 +15,18 @@ const About = () => (
         </span>
       </h2>
       <p>
-        Vishal Yadav is a seasoned Forex trader with over 10 years of experience
+        SniperAI is a seasoned Forex trader with over 10 years of experience
         managing high-value portfolios. With a deep understanding of market
         trends and risk management, Vishal consistently achieves outstanding
         results for clients around the world.
       </p>
       <p>
-        His expertise includes trading signals, personalized coaching, and
+        Our expertise includes trading signals, personalized coaching, and
         market analysis to help traders of all levels succeed. He is passionate
         about educating new traders and promoting disciplined trading practices.
       </p>
       <p>
-        Vishal’s commitment to transparency and client success has earned him
+        Our commitment to transparency and client success has earned him
         recognition as a trusted Forex expert in international markets.
       </p>
 
